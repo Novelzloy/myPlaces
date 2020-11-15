9925b36a5fcdf600c46a1970fdf3463a50b3596d
Novel <novel_90@mail.ru>
2020-11-15 16:01:13 +0300

    App Icon Launch Screen Row High

---
+++ myPlaces/MyPlaceTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MyPlaceTableViewController.swift
+//  myPlaces
+//
+//  Created by Роман on 11.11.2020.
+//
+
+import UIKit
+
+class MyPlaceTableViewController: UITableViewController {
+    
+    let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    // MARK: - Table view data source
+
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurantNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+
+       
+
+        return cell
+    }
+    
+    // MARK: - table veiw delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+
+    /*
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
