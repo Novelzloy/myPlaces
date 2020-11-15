d0fddc5b4445b29918e2a2466f52caf9998698ab
Novel <novel_90@mail.ru>
2020-11-15 18:07:38 +0300

    Custom Table View Cell

---
+++ myPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  myPlaces
+//
+//  Created by Роман on 15.11.2020.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var typeLable: UILabel!
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var nameLable: UILabel!
+    
+}
